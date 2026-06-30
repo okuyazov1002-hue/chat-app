ce343fb59ab009b60d0b41547f92dbda129b38dc
okuyazov1002-hue <o.kuyazov.1002@gmail.com>
2026-06-30 09:44:30 +0000

    sync src folder with main files

--- src/users.js
+++ src/users.js
@@ -2,6 +2,7 @@ const users = [
   { username: 'admin', password: '1234', name: 'Администратор' },
   { username: 'I.kenjibaev', password: '1234', name: 'Пользователь 1' },
   { username: 'user2', password: 'pass2', name: 'Пользователь 2' },
+  { username: 'o.kuryazov', password: 'Enish1002', name: 'Курязов Озодбой' },
 ];
 
 function checkUser(username, password) {
